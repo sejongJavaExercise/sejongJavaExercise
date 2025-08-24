e6da628534f71e9bf2e8bc443de2c1b0896fe07e
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:43:18 +0900

    Update Question1.java

--- src/chapter9/practice/question1/Question1.java
+++ src/chapter9/practice/question1/Question1.java
@@ -1,4 +1,26 @@
-package chapter9.practice.question1;
+public class EqualsOverride {
+    public static void main(String[] args) {
+        Paper p = new Paper(10, 20);
+        Paper q = new Paper(10, 20);
 
-public class Question1 {
+        System.out.println(p.equals(q));
+    }
+}
+
+class Paper {
+    int width;
+    int height;
+
+    Paper(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Paper paper = (Paper) obj;
+        return this.width == paper.width && this.height == paper.height;
+    }
 }
