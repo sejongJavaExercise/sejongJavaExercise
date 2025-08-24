95014b35b769b31269fba7295e937926d94e14b4
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:31:18 +0900

    Update Question16.java

--- src/chapter7/practice/question16/Question16.java
+++ src/chapter7/practice/question16/Question16.java
@@ -1,17 +1,73 @@
-package chapter7.practice.question16;
+abstract class Car {
+    abstract void call();
+}
 
-public class Question16 {
-    public static void main(String[] args) {
-        Event event = new Event() {
-            public void alertEvent() {
-                System.out.println("이벤트가 발생했습니다.");
-            }
-        };
-        event.alertEvent();
+interface Movable {
+    void move(int x, int y);
+}
+
+interface Stoppable {
+    void stop();
+}
+
+class Taxi extends Car implements Movable, Stoppable {
+    public void call() {
+        System.out.println("택시가 호출되었습니다.");
+    }
+
+    public void move(int x, int y) {
+        System.out.println("택시가 (" + x + ", " + y + ")으로 이동합니다.");
+    }
+
+    public void stop() {
+        System.out.println("택시가 정지합니다.");
     }
 }
 
-interface Event{
-    //이벤트 내용을 println() 메서드로 출력
-    void alertEvent();
+class Truck extends Car implements Movable, Stoppable {
+    public void call() {
+        System.out.println("트럭이 호출되었습니다.");
+    }
+
+    public void move(int x, int y) {
+        System.out.println("트럭이 (" + x + ", " + y + ")으로 이동합니다.");
+    }
+
+    public void stop() {
+        System.out.println("트럭이 정지합니다.");
+    }
+}
+
+class Bus extends Car implements Movable, Stoppable {
+    public void call() {
+        System.out.println("버스가 호출되었습니다.");
+    }
+
+    public void move(int x, int y) {
+        System.out.println("버스가 (" + x + ", " + y + ")으로 이동합니다.");
+    }
+
+    public void stop() {
+        System.out.println("버스가 정지합니다.");
+    }
+}
+
+public class TaxiTruckBus {
+    public static void main(String[] args) {
+        Taxi taxi = new Taxi();
+        Truck truck = new Truck();
+        Bus bus = new Bus();
+
+        taxi.call();
+        taxi.move(100, 200);
+        taxi.stop();
+
+        truck.call();
+        truck.move(200, 100);
+        truck.stop();
+
+        bus.call();
+        bus.move(300, 300);
+        bus.stop();
+    }
 }
