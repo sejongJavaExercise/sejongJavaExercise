4d29488afa2ba5416785c01d09811c17d20976e1
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:43:45 +0900

    Update Question2.java

--- src/chapter9/practice/question2/Question2.java
+++ src/chapter9/practice/question2/Question2.java
@@ -1,22 +1,19 @@
-package chapter9.practice.question2;
-
-public class Question2 {
+public class ToStringOverride {
     public static void main(String[] args) {
         Paper p = new Paper(10, 20);
 
-        System.out.println(p.toString());
+        System.out.println(p.toString()); 
     }
 }
 
-class Paper{
+class Paper {
     int width;
     int height;
-    Paper(int width, int height){
+
+    Paper(int width, int height) {
         this.width = width;
         this.height = height;
     }
-
-    //여기에 toString() 재정의
 
     @Override
     public String toString() {
