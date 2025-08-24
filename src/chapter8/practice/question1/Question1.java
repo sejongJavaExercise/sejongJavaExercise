1467632db571644a476bb8d579b5eac84443b6bc
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:37:15 +0900

    Update Question1.java

--- src/chapter8/practice/question1/Question1.java
+++ src/chapter8/practice/question1/Question1.java
@@ -1,26 +1,12 @@
-package chapter8.practice.question1;
-
-import java.util.Scanner;
-
-public class Question1 {
+public class ArrayIndex {
     public static void main(String[] args) {
-        System.out.println("프로그램을 시작합니다.");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("예외를 발생시키겠습니까? (y / n) ");
-        String answer = scanner.nextLine();
-        if(answer.equals("y")){
-            System.out.println("예외를 발생시킵니다...");
-            try{
-                throw new Exception();
-            }
-            catch(Exception e){
-                System.out.println("예외가 발생했습니다.");
-            }
+        int[] numbers = {1, 2, 3, 4, 5};
+        int index = 10;
+
+        try {
+            System.out.println(numbers[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("예외 발생: " + e.getMessage());
         }
-        else{
-            System.out.println("예외를 발생시키지 않습니다...");
-        }
-        System.out.println("프로그램을 종료합니다.");
     }
 }
-
