c89248a4ac76e829328f47e124ce31e1f4a615a1
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:36:10 +0900

    Delete src/chapter7/practice/Question5.java

--- src/chapter7/practice/Question5.java
+++
@@ -1,9 +0,0 @@
-package chapter7.practice;
-
-public class Question5 {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        int x = scanner.nextInt();
-        System.out.println(x);
-    }
-}
