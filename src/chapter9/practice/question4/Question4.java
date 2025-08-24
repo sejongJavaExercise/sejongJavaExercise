11f6df85854e3a4ff84e467eedc7d65a660913e3
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:46:18 +0900

    Update Question4.java

--- src/chapter9/practice/question4/Question4.java
+++ src/chapter9/practice/question4/Question4.java
@@ -1,23 +1,13 @@
-package chapter9.practice.question4;
-
 import java.util.Scanner;
 
-public class Question4 {
+public class ChangeCase {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        StringBuffer stringBuffer = new StringBuffer("");
-        for (int i = 0; i < n; i++) {
-            if(i % 3 == 0){
-                stringBuffer.append("엄");
-            }
-            else if(i % 3 == 1){
-                stringBuffer.append("준");
-            }
-            else{
-                stringBuffer.append("식");
-            }
-        }
-        System.out.println(stringBuffer);
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.toUpperCase());
+
+        System.out.println(str.toLowerCase());
     }
 }
