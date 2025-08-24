02a8f38bcd3a6c89bc3809f3ba9f78cfa169596e
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:39:57 +0900

    Update Question7.java

--- src/chapter8/practice/question7/Question7.java
+++ src/chapter8/practice/question7/Question7.java
@@ -1,33 +1,23 @@
-package chapter8.practice.question7;
-
 import java.util.Scanner;
 
-public class Question7 {
+public class TryCatchFinally {
     public static void main(String[] args) {
-        try{
-            method();
-        }
-        catch(NullPointerException e){
-            System.out.println("main에서 NullPointerException이 처리되었습니다.");
-        }
-    }
-
-    static void method() throws NullPointerException{
+        System.out.println("프로그램을 시작합니다.");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("어떤 예외를 발생시키겠습니까?");
-        System.out.println("1. ArithmeticException");
-        System.out.println("2. NullPointerException");
-        int answer = scanner.nextInt();
-        try{
-            if(answer == 1){
-                throw new ArithmeticException();
+        System.out.print("예외를 발생시키겠습니까? (y / n) ");
+        String answer = scanner.nextLine();
+
+        try {
+            if (answer.equals("y")) {
+                System.out.println("예외를 발생시킵니다...");
+                throw new Exception();
+            } else {
+                System.out.println("예외를 발생시키지 않습니다...");
             }
-            else{
-                throw new NullPointerException();
-            }
-        }
-        catch (ArithmeticException e){
-            System.out.println("method에서 ArithmeticException이 처리되었습니다.");
+        } catch (Exception e) {
+            System.out.println("예외가 발생했습니다.");
+        } finally {
+            System.out.println("프로그램을 종료합니다.");
         }
     }
 }
