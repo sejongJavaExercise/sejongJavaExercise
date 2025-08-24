a3d5579457fca7a921b4fbdde8b6698823b38c41
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:40:51 +0900

    Update Question9.java

--- src/chapter8/practice/question9/Question9.java
+++ src/chapter8/practice/question9/Question9.java
@@ -1,45 +1,18 @@
-package chapter8.practice.question9;
-
-import java.util.Scanner;
-
-public class Question9 {
+public class MethodAndMain {
     public static void main(String[] args) {
         try {
             method();
-        }
-        catch(Question9Exception e){
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("main에서 Exception이 처리되었습니다.");
         }
     }
 
-    static void method()throws Question9Exception{
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("어떤 예외를 발생시키겠습니까?");
-        System.out.println("1. ArithmeticException");
-        System.out.println("2. NullPointerException");
-        System.out.println("3. ClassNotFoundException");
-        int answer = scanner.nextInt();
-        try{
-            if(answer == 1){
-                throw new ArithmeticException("ArithmeticException 발생");
-            }
-            else if(answer == 2){
-                throw new NullPointerException("NullPointerException 발생");
-            }
-            else{
-                throw new ClassNotFoundException("ClassNotFoundException 발생");
-            }
+    static void method() throws Exception {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("method에서 Exception이 처리되었습니다.");
+            throw e;
         }
-        catch(Exception e){
-            Question9Exception question9Exception = new Question9Exception("Question9Exception 발생");
-            question9Exception.initCause(e);
-            throw question9Exception;
-        }
-    }
-}
-
-class Question9Exception extends Exception{
-    Question9Exception(String message){
-        super(message);
     }
 }
