af1e69256bc4c5f358eb9ad663dedc0853d994ae
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:39:00 +0900

    Update Question4.java

--- src/chapter8/practice/question4/Question4.java
+++ src/chapter8/practice/question4/Question4.java
@@ -1,18 +1,23 @@
-package chapter8.practice.question4;
+import java.util.Scanner;
 
-public class Question4 {
+public class ThrowExceptionEx {
     public static void main(String[] args) {
-        method1();
-    }
+        try {
+            System.out.println("프로그램을 시작합니다.");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("예외를 발생시키겠습니까? (y / n) ");
+            String answer = scanner.nextLine();
 
-    static void method1(){
-        try{
-            method2();
+            if (answer.equals("y")) {
+                System.out.println("예외를 발생시킵니다...");
+                throw new Exception();
+            } else {
+                System.out.println("예외를 발생시키지 않습니다...");
+            }
+        } catch (Exception e) {
+            System.out.println("예외가 발생했습니다.");
+        } finally {
+            System.out.println("프로그램을 종료합니다.");
         }
-        catch(Exception e){}
-    }
-
-    static void method2() throws Exception{
-        throw new Exception();
     }
 }
