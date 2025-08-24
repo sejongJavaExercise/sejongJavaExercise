7a42a7c8039e08b09ba4433155f0b04adc17dc23
JiUk Kim <48274871+jiuk-cpf19c@users.noreply.github.com>
2025-08-24 17:40:18 +0900

    Update Question8.java

--- src/chapter8/practice/question8/Question8.java
+++ src/chapter8/practice/question8/Question8.java
@@ -1,22 +1,27 @@
-package chapter8.practice.question8;
+import java.util.Scanner;
 
-public class Question8 {
+public class MethodMain {
     public static void main(String[] args) {
-        try{
+        try {
             method();
-        }
-        catch(Exception e){
-            System.out.println("main에서 Exception이 처리되었습니다.");
+        } catch (NullPointerException e) {
+            System.out.println("main에서 NullPointerException이 처리되었습니다.");
         }
     }
 
-    static void method() throws Exception{
-        try{
-            throw new Exception();
-        }
-        catch(Exception e){
-            System.out.println("method에서 Exception이 처리되었습니다.");
-            throw e;
+    static void method() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요 (1 또는 2): ");
+        int num = scanner.nextInt();
+
+        if (num == 1) {
+            try {
+                throw new ArithmeticException("Arithmetic 예외 발생");
+            } catch (ArithmeticException e) {
+                System.out.println("method에서 ArithmeticException이 처리되었습니다.");
+            }
+        } else if (num == 2) {
+            throw new NullPointerException("NullPointer 예외 발생");
         }
     }
 }
